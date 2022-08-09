65afe51241ae58a9c6062ee1182e75ef2623ae58
nadia12-p <nadiapintos98@gmail.com>
2022-08-09 14:08:39 -0500

    multiple varible message

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-var nombre = "Nadia"
+var nombre = "Nadia";
+nombre = "Jose";
 
-alert(nombre)
+var edad = 30;
+var telefono = "52 9211474645"
+
+var mensajeFinal = "Nombre: " + nombre + ". Edad: " + edad + ". Tel: " + telefono; 
+
+alert(mensajeFinal);
